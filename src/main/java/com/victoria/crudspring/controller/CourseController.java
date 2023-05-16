92d6e2000614fe9b30ca5a15ab2c6ff159420c1e
Victória Alves <vic.alves200216@gmail.com>
2023-05-16 11:57:38 -0300

    Melhorando status Post

--- src/main/java/com/victoria/crudspring/controller/CourseController.java
+++ src/main/java/com/victoria/crudspring/controller/CourseController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victoria.crudspring.model.Course;
@@ -31,9 +33,10 @@ public class CourseController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Course> salvar(@RequestBody Course course) {
-		//return courseRepository.save(course);
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(courseRepository.save(course));
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Course salvar(@RequestBody Course course) {
+		return courseRepository.save(course);
+//		return ResponseEntity.status(HttpStatus.CREATED)
+//				.body(courseRepository.save(course));    ---melhor usado quando precisa enviar uma mensagem personalizada
 	}
 }
