5e417920ec9c4b0c78d59fbfab19507c76bcc3e3
Victória Alves <vic.alves200216@gmail.com>
2023-05-16 11:55:24 -0300

    Criando metodo salvar

--- src/main/java/com/victoria/crudspring/controller/CourseController.java
+++ src/main/java/com/victoria/crudspring/controller/CourseController.java
@@ -3,7 +3,11 @@ package com.victoria.crudspring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,12 @@ public class CourseController {
 	@GetMapping
 	public List<Course> listar(){
 		return courseRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Course> salvar(@RequestBody Course course) {
+		//return courseRepository.save(course);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(courseRepository.save(course));
 	}
 }
