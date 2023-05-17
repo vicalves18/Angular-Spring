1e12ca8b9b67fcd7a222356f538cc2fb46fb9b4e
Victória Alves <vic.alves200216@gmail.com>
2023-05-17 09:40:46 -0300

    Buscando id

--- src/main/java/com/victoria/crudspring/controller/CourseController.java
+++ src/main/java/com/victoria/crudspring/controller/CourseController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +30,13 @@ public class CourseController {
 	@GetMapping
 	public List<Course> listar(){
 		return courseRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findById(@PathVariable Long id) {
+		return courseRepository.findById(id)
+				.map(result -> ResponseEntity.ok().body(result))
+				.orElse( ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping
