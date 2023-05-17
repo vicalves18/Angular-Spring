2dee53615c4f7652cad08a0e79ad63db4a63926b
Victória Alves <vic.alves200216@gmail.com>
2023-05-17 11:36:31 -0300

    Método Atualizar

--- src/main/java/com/victoria/crudspring/controller/CourseController.java
+++ src/main/java/com/victoria/crudspring/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,5 +46,17 @@ public class CourseController {
 		return courseRepository.save(course);
 //		return ResponseEntity.status(HttpStatus.CREATED)
 //				.body(courseRepository.save(course));    ---melhor usado quando precisa enviar uma mensagem personalizada
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Course> atualizar(@PathVariable Long id , @RequestBody Course course) {
+		return courseRepository.findById(id)
+				.map(result -> { 
+					result.setNome(course.getNome());
+					result.setCategoria(course.getCategoria());
+					Course atualiza = courseRepository.save(result);
+					return ResponseEntity.ok().body(atualiza);
+				})
+				.orElse( ResponseEntity.notFound().build());
 	}
 }
